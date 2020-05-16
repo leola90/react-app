42fd387d7ac438f5d2d417ccf749e11f6557e49d
Leo La <leola90@gmail.com>
2020-05-15 22:58:18 -0500

    Updates

---
+++ precache-manifest.70c38227dd0aee3a50a8c3cc81ddd4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b23125855bfc2b7f30063e50fdfeaf",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "e676778421c94982469d",
+    "url": "/react-app/static/css/main.32467ec2.chunk.css"
+  },
+  {
+    "revision": "28ab5cff5d654e30c8fa",
+    "url": "/react-app/static/js/2.82bd998f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.82bd998f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e676778421c94982469d",
+    "url": "/react-app/static/js/main.402dce3a.chunk.js"
+  },
+  {
+    "revision": "13037bcfcabf974fe7ff",
+    "url": "/react-app/static/js/runtime-main.3db46310.js"
+  }
+]);
